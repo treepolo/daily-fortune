a32caa9038b5cebd6271d2bf3d10491f71c8a9db
treepolo <bigtreepolo@gmail.com>
2026-09-03 19:08:02 +0800

    feat: track foreground sessions from activity lifecycle

--- app/src/main/java/com/treepolo/dailyfortune/MainActivity.kt
+++ app/src/main/java/com/treepolo/dailyfortune/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import com.treepolo.dailyfortune.ui.DailyFortuneRoot
+import com.treepolo.dailyfortune.ui.FortuneViewModel
 
 class MainActivity : ComponentActivity() {
+    private val fortuneViewModel: FortuneViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DailyFortuneRoot()
+            DailyFortuneRoot(fortuneViewModel)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        fortuneViewModel.onForeground()
+    }
+
+    override fun onStop() {
+        fortuneViewModel.onBackground()
+        super.onStop()
     }
 }
